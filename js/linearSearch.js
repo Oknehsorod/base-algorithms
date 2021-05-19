5cc239293a00141748d92602b8e9ffef29f6598a
Igor Doroshenko <oknehsorod@gmail.com>
2021-05-19 13:47:05 +0300

    Add linear search functions for js

---
+++ js/linearSearch.js
@@ -0,0 +1,21 @@
+function linearSearch(array, element) {
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === element) {
+      return i;
+    }
+  }
+
+  return NaN;
+}
+
+function linearSearchRecursively(array, element) {
+  if (array.length === 0) {
+    return NaN;
+  }
+
+  if (array[0] === element) {
+    return 0 
+  }
+
+  return 1 + linearSearchRecursively(array.slice(1), element)
+}
